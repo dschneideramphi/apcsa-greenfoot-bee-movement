e7cf75740701de489cb2f1b300c8fc83a9a6fbb6
dschneideramphi <dschneider@amphi.com>
2019-08-22 16:48:32 -0700

    Added Files

---
+++ bee.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bee here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bee extends Actor
+{
+    /**
+     * Act - do whatever the bee wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        turn(Greenfoot.getRandomNumber(10) - 5);
+        move(5);
+        int x = getX();
+        int y = getY();
+        if(x > 600 || y < 0 || y > 400) {
+            x = 0;
+            y = Greenfoot.getRandomNumber(400);
+            setRotation(0);
+            setLocation(x, y);
+        }
+    }    
+}
